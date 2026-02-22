1f6cde48eda1119db06e032babe68ac1b5c71f23
QwQ3094-Live <caonimen587@gmail.com>
2026-02-22 08:59:02 +0800

    Add files via upload

--- app/src/main/java/com/par9uet/jm/ui/screens/tabScreen/BottomNavigationBarComponent.kt
+++ app/src/main/java/com/par9uet/jm/ui/screens/tabScreen/BottomNavigationBarComponent.kt
@@ -2,6 +2,7 @@ package com.par9uet.jm.ui.screens.tabScreen
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
